259b4fef7e1fadd40a945bd6e47b50bff625c888
younghun <asqwklop12@naver.com>
2021-08-26 00:53:07 +0900

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+
+  - 베너 만드는 사이트
+    - https://devops.datenkollektiv.de/banner.txt/index.html
+  - 
---
+++ src/test/java/com/hole/ex1/Ex1ApplicationTests.java
@@ -0,0 +1,16 @@
+package com.hole.ex1;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+
+@SpringBootTest
+class Ex1ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
