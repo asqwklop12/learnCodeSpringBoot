bfab8f449a3c50aea906c9dc3dfc2ca42d1345e1
younghun <asqwklop12@naver.com>
2021-08-28 00:14:23 +0900

    컨트롤러 생성

--- src/main/java/com/hole/ex1/controller/SampleController.java
+++ src/main/java/com/hole/ex1/controller/SampleController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class SampleController {
-  
+
   @GetMapping("/hello")
   public String[] hello() {
     return new String[]{"Hello", "World"};
